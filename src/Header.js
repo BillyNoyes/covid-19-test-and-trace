e7dde5c0989a04be555bb84ef1f880c419fe3e87
William (Billy) Noyes <wbillypn@gmail.com>
2020-09-04 16:22:44 +0100

    Added span tag to emojis

--- src/Header.js
+++ src/Header.js
@@ -6,13 +6,18 @@ function Header() {
     <div className="header">
       <h1>Covid-19 Test & Trace</h1>
       <p>
-        ✏️ <br /> By filling out this form, you are helping us provide vital
-        Test and Trace information to the NHS.
+        <span role="img" aria-label="pencil">
+          ✏️
+        </span>{" "}
+        <br /> By filling out this form, you are helping us provide vital Test
+        and Trace information to the NHS.
       </p>
       <p>
-        🔒 <br /> Any data you share will be stored securely and held
-        temporarily. We will never share or use your data anywhere outside this
-        programme.
+        <span role="img" aria-label="lock">
+          🔒
+        </span>{" "}
+        <br /> Any data you share will be stored securely and held temporarily.
+        We will never share or use your data anywhere outside this programme.
       </p>
     </div>
   );
