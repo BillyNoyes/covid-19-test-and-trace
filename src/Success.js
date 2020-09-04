bf46b9f4340ed4e05f052590fa896e0299aeece8
William (Billy) Noyes <wbillypn@gmail.com>
2020-09-04 03:28:23 +0100

    Created Success component

---
+++ src/Success.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Success.css";
+
+function Success() {
+  return (
+    <div className="success">
+      <p>
+        <strong>Thank you</strong>
+        <br /> For your participation in Test and Trace
+      </p>
+    </div>
+  );
+}
+
+export default Success;
