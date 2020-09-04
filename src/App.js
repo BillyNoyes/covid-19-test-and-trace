4cb91c4b1c13d591161e28e880282a0505133cb7
William (Billy) Noyes <wbillypn@gmail.com>
2020-09-04 16:22:24 +0100

    Added sign up button for businesses

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import "./App.css";
 import Form from "./Form";
 import Header from "./Header";
 import Success from "./Success";
+import { Button } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
+      <div className="app__contactButton">
+        <Button className="form__button" variant="outlined">
+          Sign Up Your Business
+        </Button>
+      </div>
       <Router>
         <Switch>
           <Route path="/success">
